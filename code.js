a19f188e3a773a94c615c4d5db6a7b53c090a44e
Rauch Rodrigues <94990103+rauchrods@users.noreply.github.com>
2023-03-25 19:20:28 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,107 @@
+var puppeteer = require("puppeteer");
+
+
+
+
+
+(async () => {
+
+
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto("https://github.com/trending");
+      
+ 
+    let ansobj={
+        repositories:[],
+        developers:[]
+    }
+
+    await page.waitForSelector("body"); 
+    await page.waitForSelector(".Box");
+    await page.waitForSelector(".Box-row"); 
+
+    let arrrepoobj = await page.evaluate(() => {
+        let repoarr = document.querySelectorAll(".Box .Box-row");
+
+        repoarr.forEach((element) => {
+            console.log(element.textContent);
+        });
+        
+        let arrrepoobj = [];
+        for (let val of repoarr) {
+
+            let obj = {
+                
+                title: val.querySelector(".h3 > a ").textContent.trim(),
+                desciption: val.querySelector("p").textContent.trim(),
+                url: val.querySelector(".h3>a").getAttribute("href"),
+                stars : val.querySelector("div.f6.color-fg-muted.mt-2 > a:nth-child(2)").innerText.trim(),
+                today_stars : val.querySelector("div.f6.color-fg-muted.mt-2 > span.d-inline-block.float-sm-right").textContent.trim(),
+                forks: val.querySelector("div.f6.color-fg-muted.mt-2 > a.Link--muted.d-inline-block.mr-3:nth-of-type(2)").innerText.trim(),             
+                language: val.querySelector(".f6 > span").textContent.trim(),
+
+            }
+
+            arrrepoobj.push(obj);
+
+        }
+        return arrrepoobj;
+
+    });
+    ansobj["repositories"]=arrrepoobj;
+
+    console.log("Repos are", arrrepoobj);
+
+   
+
+  let buttonobj =   await page.evaluate(()=>{
+    //    return document.querySelector("div.Box-header.d-md-flex.flex-items-center.flex-justify-between > nav > a:nth-child(2)").click();
+       return document.querySelector(".Box-header > nav > a:nth-child(2)").click();
+
+        //devloper button clicked
+    });
+
+
+   
+
+    // // await page.waitForSelector(".Box");
+    // // await page.waitForSelector("#select-menu-language");
+
+    
+    // await page.evaluate(()=>{
+        //   document.querySelector("#select-menu-language > summary > span").click();
+        //    await   page.type("#select-menu-language > details-menu > div.select-menu-filters > filter-input > input", "javascript" , {delay:200});
+        //    await document.querySelector("#languages-menuitems > div > a:nth-child(287)").click();
+            //devloper button clicked
+       
+    // });
+
+
+
+    // await page.waitForSelector("body > div.logged-in.env-production.page-responsive > div.application-main > main > div.position-relative.container-lg.p-responsive.pt-6 > div ");
+
+
+    //to get developer list
+    // await page.goto("https://github.com/trending");
+
+
+
+   
+     
+   
+   
+    
+
+    // await page.type("#your-repos-filter", "f1" , {delay:100});
+
+
+    console.log(ansobj);
+    
+
+    //   await browser.close();
+})();
+
+
+
+
